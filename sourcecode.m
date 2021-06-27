da33aa78f8edb3b3386b94ea052b51673dd2794d
Hamdi Boukamcha <contact@matlab-recognition-code.com>
2021-06-27 22:26:10 +0200

    Update sourcecode.m

--- sourcecode.m
+++ sourcecode.m
@@ -6,13 +6,6 @@
 % 
 %http://matlab-recognition-code.com/facial-expression-recognition-matlab-code/
 % 
-% For more informations please email me hamdouchhd@hotmail.com
-% Hamdi Boukamcha
-% Sousse
-% 4081
-% Tunisia
-% email  hamdouchhd@hotmail.com
-% mobile +21650674269
 % website http://matlab-recognition-code.com
 %
 %
